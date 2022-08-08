89c910b10263bb09eae1b6fc3246f0943afab888
Maria Lina <marialinacdallagnol@gmail.com>
2022-08-08 16:40:54 -0300

    siteprism2

--- siteprism/features/support/env.rb
+++ siteprism/features/support/env.rb
@@ -8,5 +8,18 @@ ENVIRONMENT = ENV['ENVIRONMENT']
 ENVIRONMENT_CONFIG = YAML.load_file(File.dirname(__FILE__) + "/environment/#{ENVIRONMENT}.yml")
 URL = ENVIRONMENT_CONFIG['url']
 
-Capybara.default_driver = :selenium_chrome
+Capybara.register_driver :my_chrome do |app|
+    caps = Selenium::WebDriver::Remote::Capabilities.chrome("goog:chromeOptions" => 
+        {"args" => ["--incognito", "--start-maximized", "--window-size=1420,835"]})
+
+    if ENV['HEADLESS']
+        caps['goog:chromeOptions']['args'] << '--headless'
+    end
+    options = { browser: chrome, desired_capabilities: caps }
+    Capybara::Selenium::Driver.new(app, options)
+
+    
+end
+
+Capybara.default_driver = :selenium_chrome 
 Capybara.app_host = URL
