f974949214ca33a905785ca14a76eb5e4f8c32b3
Maria Lina <marialinacdallagnol@gmail.com>
2022-08-08 17:15:20 -0300

    config

--- siteprism/features/support/env.rb
+++ siteprism/features/support/env.rb
@@ -10,16 +10,21 @@ URL = ENVIRONMENT_CONFIG['url']
 
 Capybara.register_driver :my_chrome do |app|
     caps = Selenium::WebDriver::Remote::Capabilities.chrome("goog:chromeOptions" => 
-        {"args" => ["--incognito", "--start-maximized", "--window-size=1420,835"]})
+        {"args" => ["--incognito", "--start-maximized", "--window-size=1420,835", "--ignore-ssl-errors","--ignore-certificate-errors","--disable-popup-blocking",
+            "--no-sandbox","--acceptInsecureCerts=true", "--disable-impl-side-painting", "--debug_level=3", "--disable-gpu", "--disable-translate"]})
 
     if ENV['HEADLESS']
         caps['goog:chromeOptions']['args'] << '--headless'
+        caps['goog:chromeOptions']['args'] << '--disable-site-isolation-trials'
     end
-    options = { browser: chrome, desired_capabilities: caps }
-    Capybara::Selenium::Driver.new(app, options)
+    client = Selenium::WebDriver::Remote::Http::Default.new
+    client.read_timeout = 90
+    options = { browser: chrome, desired_capabilities: caps, http_client: client}
 
-    
+    puts 'Raising driver'
+    Capybara::Selenium::Driver.new(app, options)
 end
 
 Capybara.default_driver = :selenium_chrome 
 Capybara.app_host = URL
+Capybara.default_max_wait_time = 10
