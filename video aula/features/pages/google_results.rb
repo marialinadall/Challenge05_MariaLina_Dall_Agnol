ce9eba05f9b28945dad29acc2ab3ee08a033d709
Maria Lina <marialinacdallagnol@gmail.com>
2022-08-08 13:05:56 -0300

    video aulas

---
+++ video aula/features/pages/google_results.rb
@@ -0,0 +1,4 @@
+
+class GoogleResults < BasePage
+
+end 
---
+++ video aula/features/support/env.rb
@@ -0,0 +1,7 @@
+require 'cucumber'
+require 'capybara/cucumber'
+require 'rspec'
+require 'selenium-webdriver' 
+
+Capybara.default_driver = :selenium_chrome
+Capybara.app_host = 'http://www.google.com.br'
