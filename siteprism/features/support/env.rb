43822853dbac71397eddf5193189bf6ad34e9fac
Maria Lina <marialinacdallagnol@gmail.com>
2022-08-08 15:43:25 -0300

    siteprism

--- siteprism/features/support/env.rb
+++ siteprism/features/support/env.rb
@@ -4,5 +4,9 @@ require 'rspec'
 require 'selenium-webdriver'
 require 'site_prism'
 
+ENVIRONMENT = ENV['ENVIRONMENT']
+ENVIRONMENT_CONFIG = YAML.load_file(File.dirname(__FILE__) + "/environment/#{ENVIRONMENT}.yml")
+URL = ENVIRONMENT_CONFIG['url']
+
 Capybara.default_driver = :selenium_chrome
-Capybara.app_host = 'https://www.google.com.br'
+Capybara.app_host = URL
